ac159939193f39f2e8650a044804af5f3600939e
davi lucas <davi-06@hotmail.com>
2023-07-11 11:54:48 -0300

    montagem de paginas iniciais do aplicativo

---
+++ calculadoraImc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculadoraImc
+//
+//  Created by Davi Lucas Gomes Feitosa on 11/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfWeigtht: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
+}
+
